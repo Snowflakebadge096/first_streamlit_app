d9e1ee1166ba75bf5e7ab5b7e1ba87d633d579e9
Snowflakebadge096 <153214755+Snowflakebadge096@users.noreply.github.com>
2023-12-07 10:48:11 -0600

    Update streamlit_app.py
    fruit shower

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 # Display the table on the page.
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.dataframe(my_fruit_list)
