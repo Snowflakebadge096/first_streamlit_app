49448f45560df1fa3818848066f0ccc9155fa41d
Snowflakebadge096 <153214755+Snowflakebadge096@users.noreply.github.com>
2023-12-07 14:31:14 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,15 +42,6 @@ except URLError as e:
     streamlit.error()
 #streamlit.write('The user entered ', fruit_choice)
 
-
-
-# write your own comment -what does the next line do? 
-
-# write your own comment - what does this do?
-
-
-
-
 streamlit.header("The fruit load list contains")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur: 
@@ -61,10 +52,16 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur
+    my_cur.execute("insert into fruit_load_list values ('frome streamlit')")
+  return "Thanks for adding " + new_fruit
 
-streamlit.stop()
+add_my_fruit = streamlit.text_input('what fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function= inster_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
 
-fruit_choice = streamlit.text_input('what fruit would you like to add?','')
-streamlit.write('Thanks for adding ', fruit_choice)
 
-my_cur.execute("insert into fruit_load_list values ('frome streamlit')")
+
